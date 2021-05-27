a58ce547abdd17f3c09cb2592ed2ddef4e756d16
Jesper <jesp73g2@stud.kea.dk>
2021-05-28 01:41:59 +0200

    blandet guf

--- src/main/java/dpcm/dpcmgr4jbranch/controller/FrontController.java
+++ src/main/java/dpcm/dpcmgr4jbranch/controller/FrontController.java
@@ -2,6 +2,7 @@ package dpcm.dpcmgr4jbranch.controller;
 
 import dpcm.dpcmgr4jbranch.dataAccesLayer.DataFacade;
 import dpcm.dpcmgr4jbranch.model.classes.Project;
+import dpcm.dpcmgr4jbranch.model.classes.SubTask;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,57 +22,55 @@ public class FrontController {
     @Autowired
     DataFacade dataFacade;
     ArrayList<Project> projectArrayList = new ArrayList<>();
-    ArrayList<String> activeProjectsArray = new ArrayList<>();
+    ArrayList<SubTask> subTaskArrayList = new ArrayList<>();
     //DataFacade dataFacade = new DataFacade();
 
 
     // @GetMapping tager imod en getrequest fra browseren.
     @GetMapping("/index")
-    public String index()
-    {
+    public String index() {
         return "index";
     }
+
     // index er en html fil der returneres. Og altid den html der ledes efter først!
     // login er den html fil der returneres (get).
     @GetMapping("/login")
-    public String login()
-    {
+    public String login() {
         return "login";
     }
 
     // Hvis login er godkendt bliver vi redirectet til project_list
     @PostMapping("/submit_login")
-    public String submit_login()
-    {
+    public String submit_login() {
         return "redirect:project_list";
     }
 
     // Skal indeholde en samlet liste over alle de projekter der er blevet arbejdet, og ikke er slettet fra DB. 'overblik'
     @GetMapping("/project_creator")
-    public String project_creator(WebRequest request, Model model)throws Exception
-    {
+    public String project_creator(WebRequest request, Model model) throws Exception {
 
 
         //int projectId = dataFacade.createProject(projectName, consultantName, projectDescription, startDate, deadLine);
         //Project project = dataFacade.getProjectFromId(projectId);
         return "project_creator";
     }
-     @GetMapping("/project_list")
-    public String project_list(Model model)
-    {
-        model.addAttribute("projects",projectArrayList);
+
+    @GetMapping("/project_list")
+    public String project_list(Model model) {
+        model.addAttribute("projects", projectArrayList);
 
         return "project_list";
     }
 
     // @PostMapping tager imod en Post-request fra browseren. åbner project_creator.
     @PostMapping("/submit_project_form")
-    public String submit_project_form(WebRequest request) {
+    public String submit_project_form(WebRequest request) throws Exception {
         String projectName = request.getParameter("project_name");
         String consultantName = request.getParameter("consultant_name");
         String projectDescription = request.getParameter("project_description");
         String startDate = request.getParameter("start_date");
         String deadLine = request.getParameter("dead_line");
+        dataFacade.createProject(projectName, projectDescription, consultantName, startDate, deadLine);
         /*@RequestParam("consultant_name")String consultantName
         , @RequestParam("project_name")String projectName
         , @RequestParam("start_date")String startDate
@@ -81,29 +80,36 @@ public class FrontController {
         , @RequestParam("sub_task_description") String subTaskDescription
         , @RequestParam("sub_task_time")String subTaskTime)*/
 
-        Project project = new Project(projectName,consultantName,projectDescription,startDate,deadLine);
+        Project project = new Project(projectName, consultantName, projectDescription, startDate, deadLine);
         projectArrayList.add(project);
         return "redirect:/project_creation_succes";
     }
-       @GetMapping("/project_creation_succes")
-        public String project_creation_succes(Model model){
-        model.addAttribute("project_details",projectArrayList.get(projectArrayList.size()-1));
+
+    @GetMapping("/project_creation_succes")
+    public String project_creation_succes(Model model) {
+        model.addAttribute("project_details", projectArrayList.get(projectArrayList.size() - 1));
         return "project_creation_succes";
     }
-        // Project details skal indeholde alle fakta om aktuelt project man arbejder med.
-        @GetMapping("/project_details")
-        public String project_details(Model model)
-        {
-            model.addAttribute("detailposts",projectArrayList);
-            return "project_details";
-        }
+
+    // Project details skal indeholde alle fakta om aktuelt project man arbejder med.
+    @GetMapping("/project_details")
+    public String project_details(Model model) {
+        model.addAttribute("detailposts", projectArrayList);
+        return "project_details";
+    }
 
     @GetMapping("/project_update")
-    public String project_update(Model model)
-    {
-        model.addAttribute("new_subtask",activeProjectsArray.get(activeProjectsArray.size()-1));
+    public String project_update() {
         return "project_update";
     }
+
+    @PostMapping("/add_subtask_form")
+    public String add_subtask_form(Model model) {
+        model.addAttribute("subtaskpost", subTaskArrayList);
+        return "redirect:/add_subtask_succes";
+    }
+
+
 
 }
 
